b80751021b1d6c1408e72e47cd8513a39b2cec51
Maxi Britez <maxi.britez@redb.ee>
2023-06-09 16:44:47 -0300

    Add setup tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
+import fetchMock from 'jest-fetch-mock';
+fetchMock.enableMocks();
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
